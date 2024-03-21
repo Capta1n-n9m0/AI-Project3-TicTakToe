758bca03301c7bd983b56932cfeca2e6618c6d6f
Abbas Aliyev <alievabbas1@gmail.com>
2024-03-21 18:17:27 +0400

    Using env for the API authentication

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-# This is a sample Python script.
+import sys
+import dotenv
+import os
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+"""
+Communication with the API is done through HTTP REST requests.
+API requires an API key and a user ID to authenticate the user.
+They are set as environment variables AI_API_KEY and AI_USER_ID.
+You can set them in the .env file in the root directory of the project or in the environment variables of your system.
+"""
 
+def main(argv: list[str]) -> None:
+    API_KEY = os.getenv("AI_API_KEY")
+    if API_KEY is None:
+        print("API_KEY is not set")
+        sys.exit(1)
+    USER_ID = os.getenv("AI_USER_ID")
+    if USER_ID is None:
+        print("USER_ID is not set")
+        sys.exit(1)
+    print(f"API_KEY: {API_KEY}")
+    print(f"USER_ID: {USER_ID}")
+    print(argv)
+    
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    dotenv.load_dotenv()
+    main(sys.argv)
+    
