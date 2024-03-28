c29d92503dfe0b077257046119f1972b24df9629
Abbas Aliyev <alievabbas1@gmail.com>
2024-03-28 19:58:17 +0400

    Arguments of CLI

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import sys
 import dotenv
 import os
-from HttpGameClient import HttpGameClient
 import argparse
 
 """
@@ -20,15 +19,88 @@ def getApiCredentials() -> tuple[str, str]:
     raise ValueError("USER_ID is not set")
   return api_key, user_id
 
+def setupArgs() -> argparse.ArgumentParser:
+  parser = argparse.ArgumentParser(description="Tic Tac Toe AI")
+  parser.add_argument(
+    "operation",
+    choices=[
+      "team",
+      "game",
+    ],
+    help="Operation to perform"
+  )
+  parser.add_argument(
+    "-c",
+    "--create",
+    action="store_true",
+    help="Create a team or game"
+  )
+  parser.add_argument(
+    "-r",
+    "--remove",
+    action="store_true",
+    help="Remove team members"
+  )
+  parser.add_argument(
+    "-l",
+    "--list",
+    action="store_true",
+    help="List teams or games"
+  )
+  parser.add_argument(
+    "-n",
+    "--name",
+    help="Name of the team"
+  )
+  parser.add_argument(
+    "-t",
+    "--team",
+    nargs="*",
+    type=int,
+    help="ID of the team"
+  )
+  parser.add_argument(
+    "-u",
+    "--user",
+    type=int,
+    nargs="*",
+    help="ID of the user"
+  )
+  parser.add_argument(
+    "-b",
+    "--board",
+    type=int,
+    help="Size of the board"
+  )
+  parser.add_argument(
+    "-s",
+    "--size",
+    type=int,
+    help="Size of the board"
+  )
+  parser.add_argument(
+    "-g",
+    "--target",
+    type=int,
+    help="Target number of consecutive marks to win"
+  )
+  return parser
+
+
 
 def main(argv: list[str]) -> None:
   api_key, user_id = getApiCredentials()
-  parser = argparse.ArgumentParser(description="Tic Tac Toe AI")
-  # Operation to create team with team name
-  parser.add_argument("--team", type=str, help="Create a team with the given name")
+  parser = setupArgs()
   
-  # Operation to add user to team
-  parser.add_argument("--add-user", type=str, help="Add a user to the team")
+  args = parser.parse_args(argv[1:])
+  if args.operation == "team":
+    ...
+  elif args.operation == "game":
+    ...
+  else:
+    raise ValueError("Invalid operation")
+  
+  
   
 
 
